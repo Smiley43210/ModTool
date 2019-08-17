d87cddd93cc1929a5b1f454adf60e727ed38e2b2
Ethan <Smiley43210@users.noreply.github.com>
2019-08-17 03:42:00 -1000

    Removed redundant update handler code

--- src/main.js
+++ src/main.js
@@ -4,78 +4,9 @@ const {app, BrowserWindow, shell} = require('electron');
 const path = require('path');
 const {ipcMain, dialog} = require('electron');
 
-if (require('electron-squirrel-startup')) app.quit();
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
-
-// this should be placed at top of main.js to handle setup events quickly
-if (handleSquirrelEvent()) {
-	// squirrel event handled and app will exit in 1000ms, so don't do anything else
-//	return;
-}
-
-function handleSquirrelEvent() {
-	if (process.argv.length === 1) {
-		return false;
-	}
-
-	const ChildProcess = require('child_process');
-	const path = require('path');
-
-	const appFolder = path.resolve(process.execPath, '..');
-	const rootAtomFolder = path.resolve(appFolder, '..');
-	const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-	const exeName = path.basename(process.execPath);
-
-	const spawn = function(command, args) {
-	let spawnedProcess, error;
-
-	try {
-		spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
-	} catch (error) {}
-		return spawnedProcess;
-	};
-
-	const spawnUpdate = function(args) {
-		return spawn(updateDotExe, args);
-	};
-
-	const squirrelEvent = process.argv[1];
-	switch (squirrelEvent) {
-		case '--squirrel-install':
-		case '--squirrel-updated':
-			// Optionally do things such as:
-			// - Add your .exe to the PATH
-			// - Write to the registry for things like file associations and
-			//   explorer context menus
-
-			// Install desktop and start menu shortcuts
-			spawnUpdate(['--createShortcut', exeName]);
-
-			setTimeout(app.quit, 1000);
-			return true;
-
-		case '--squirrel-uninstall':
-			// Undo anything you did in the --squirrel-install and
-			// --squirrel-updated handlers
-
-			// Remove desktop and start menu shortcuts
-			spawnUpdate(['--removeShortcut', exeName]);
-
-			setTimeout(app.quit, 1000);
-			return true;
-
-		case '--squirrel-obsolete':
-			// This is called on the outgoing version of your app before
-			// we update to the new version - it's the opposite of
-			// --squirrel-updated
-
-			app.quit();
-			return true;
-	}
-};
 
 function createWindow() {
 	// Create the browser window.
@@ -102,41 +33,45 @@ function createWindow() {
 	});
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-app.on('ready', () => {
-	ipcMain.on('folder-select', (event, defaultPath) => {
-		event.returnValue = dialog.showOpenDialogSync(mainWindow, {
-			title: 'Locate your Minecraft installation directory...',
-			defaultPath,
-			properties: ['openDirectory']
+if (require('electron-squirrel-startup')) {
+	app.quit();
+} else {
+	// This method will be called when Electron has finished
+	// initialization and is ready to create browser windows.
+	// Some APIs can only be used after this event occurs.
+	app.on('ready', createWindow);
+	app.on('ready', () => {
+		ipcMain.on('folder-select', (event, defaultPath) => {
+			event.returnValue = dialog.showOpenDialogSync(mainWindow, {
+				title: 'Locate your Minecraft installation directory...',
+				defaultPath,
+				properties: ['openDirectory']
+			});
+		});
+
+		ipcMain.on('show-folder', (event, openPath) => {
+			shell.openItem(openPath);
+		});
+
+		ipcMain.on('open-devtools', (event) => {
+			mainWindow.webContents.openDevTools();
 		});
 	});
-	
-	ipcMain.on('show-folder', (event, openPath) => {
-		shell.openItem(openPath);
-	});
-	
-	ipcMain.on('open-devtools', (event) => {
-		mainWindow.webContents.openDevTools();
-	});
-});
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-	// On macOS it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-});
+	// Quit when all windows are closed.
+	app.on('window-all-closed', () => {
+		// On macOS it is common for applications and their menu bar
+		// to stay active until the user quits explicitly with Cmd + Q
+		if (process.platform !== 'darwin') {
+			app.quit();
+		}
+	});
 
-app.on('activate', () => {
-	// On macOS it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
-		createWindow();
-	}
-});
+	app.on('activate', () => {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (mainWindow === null) {
+			createWindow();
+		}
+	});
+}
